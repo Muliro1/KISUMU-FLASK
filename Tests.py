86b2f873b5c9c3f023eb0caf5e50ad60f4703fdf
Muliro1 <mulirokhaemba@gmail.com>
2018-07-18 01:31:21 -0700

    Adds more tests for the api

--- Tests.py
+++ Tests.py
@@ -4,26 +4,52 @@ import unittest
 class FlaskTestCase(unittest.TestCase):
 	def setup(self):
 		tester = app.test_client(self)
+
 	def test_login(self):
 		tester = app.test_client(self)
 		response = tester.get('/api/v1/login', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)
+
+	def test_login_loads(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/login', content_type = 'html/text')
+		self.assertTrue(b'Username:' in response.data)
+
 	def test_logout(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/register', content_type = 'html/text')
+		response = tester.get('/api/v1/login', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)
-	def test_info(self):
+
+	def test_logout_loads(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/login', content_type = 'html/text')
+		self.assertTrue(b'Username:' in response.data)
+
+	def test_entries(self):
 		tester = app.test_client(self)
 		response = tester.get('/api/v1/entries/entry', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)
+
+	def test_entries_loads(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/entries/entry', content_type = 'html/text')
+		self.assertTrue(b'Entries' in response.data)
+
 	def test_home(self):
 		tester = app.test_client(self)
 		response = tester.get('/api/v1/entries/create_entry', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)
-	def test_register(self):
+
+	def test_home_loads(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/entries/create_entry', content_type = 'html/text')
+		self.assertTrue(b'entry title :' in response.data)
+
+	def test_delete(self):
 		tester = app.test_client(self)
 		response = tester.get('/api/v1/entries/delete_entry', content_type = 'html/text')
 		self.assertEqual(response.status_code, 405)
+
 
 
 if __name__ == '__main__':
