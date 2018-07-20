04c72a300f4dd6c35daffd7f660cad9f9e069642
Muliro1 <mulirokhaemba@gmail.com>
2018-07-20 00:03:04 -0700

    rectifies api

--- Tests.py
+++ Tests.py
@@ -6,24 +6,20 @@ class FlaskTestCase(unittest.TestCase):
 		tester = app.test_client(self)
 	def test_login(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/login', content_type = 'html/text')
+		response = tester.get('/api/v1/entries', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)
 	def test_logout(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/register', content_type = 'html/text')
+		response = tester.get('/api/v1/entries/create_entry', content_type = 'html/text')
 		self.assertEqual(response.status_code, 200)
 	def test_info(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/entries/entry', content_type = 'html/text')
-		self.assertEqual(response.status_code, 200)
+		response = tester.get('/api/v1/entries/<int:id>', content_type = 'html/text')
+		self.assertEqual(response.status_code, 404)
 	def test_home(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/entries/create_entry', content_type = 'html/text')
-		self.assertEqual(response.status_code, 200)
-	def test_register(self):
-		tester = app.test_client(self)
-		response = tester.get('/api/v1/entries/delete_entry', content_type = 'html/text')
-		self.assertEqual(response.status_code, 405)
+		response = tester.get('/api/v1/entries/delete_entry/<int:id>', content_type = 'html/text')
+		self.assertEqual(response.status_code, 404)
 
 
 if __name__ == '__main__':
